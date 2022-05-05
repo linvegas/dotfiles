92af2a5e85174a559af5df4ede93ed42aa84bd86
MisterCoscio <saulo.costam@gmail.com>
2022-05-05 12:49:51 -0300

    mf polybar update

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,8 +6,16 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 killall -q polybar
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    WMONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
