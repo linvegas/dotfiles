a88f461cc6d4c46fec6f1bcd77efd5655bd3a9d8
MisterConscio <saulo.costam@gmail.com>
2022-07-10 21:36:18 -0300

    trying to fix polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,9 +2,8 @@
 
 # Terminate already running bar instances
 #polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-killall -q polybar
-while pgrep -x polybar >/dev/null; do sleep 7; done
+killall -q polybar # nuclear option
+while pgrep -x polybar >/dev/null; do sleep 5; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
