f7220b8028bcad59a8661c18edb2722afad04a06
MisterConscio <saulo.costam@gmail.com>
2022-07-10 21:36:33 -0300

    few corrections

--- scripts/.local/bin/webcam.sh
+++ scripts/.local/bin/webcam.sh
@@ -2,6 +2,12 @@
 
 mpv_opt="--autofit=50%x50% --really-quiet --title='webcam'"
 
-webcam_opt="av://v4l2:/dev/video${1:-0} --demuxer-lavf-format=video4linux2 --demuxer-lavf-o-set=input_format=mjpeg,video_size=1280x720"
+case $2 in
+  480) cam_res="854x480";;
+  720) cam_res="1280x720";;
+  *) cam_res="640x480";;
+esac
+
+webcam_opt="av://v4l2:/dev/video${1:-0} --demuxer-lavf-format=video4linux2 --demuxer-lavf-o-set=input_format=mjpeg,video_size=${cam_res}"
 
 mpv $mpv_opt $webcam_opt || notify-send "Nenhuma câmera conectada"
