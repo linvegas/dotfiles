875a0a23c6a14b938dd8537d42f356e4d21ae32a
MisterConscio <saulo.costam@gmail.com>
2022-10-17 10:20:44 -0300

    minor change

--- nvim/.config/nvim/after/plugin/lspconfig.lua
+++ nvim/.config/nvim/after/plugin/lspconfig.lua
@@ -34,7 +34,7 @@ local lsp_flags = {
   debounce_text_changes = 150,
 }
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 nvim_lsp.tsserver.setup {
   on_attach = on_attach,
