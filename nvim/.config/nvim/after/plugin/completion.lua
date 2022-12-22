0e807a464d8af2d08b71a1c00883058f60fc106f
MisterConscio <saulo.costam@gmail.com>
2022-12-21 21:11:39 -0300

    better completion config

--- nvim/.config/nvim/after/plugin/completion.lua
+++ nvim/.config/nvim/after/plugin/completion.lua
@@ -4,6 +4,7 @@ if (not status) then return end
 vim.opt.completeopt = {"menu", "menuone", "noselect"}
 
 local lspkind = require('lspkind')
+local luasnip = require('luasnip')
 local cmp = require("cmp")
 
 cmp.setup({
@@ -12,7 +13,28 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
   }),
   formatting = {
     format = lspkind.cmp_format {
@@ -43,29 +65,12 @@ cmp.setup({
      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
    end,
   },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
   sources = cmp.config.sources({
     { name = 'nvim_lsp', keyword_length = 1, max_item_count = 20 },
     { name = 'path', max_item_count = 20 },
-    -- { name = 'buffer', keyword_length = 4, max_item_count = 20 },
     { name = 'luasnip', keyword_length = 1, max_item_count = 20 },
-    -- { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
   }),
 })
-
--- Set configuration for specific filetype.
---cmp.setup.filetype('gitcommit', {
---  sources = cmp.config.sources({
---    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
---  }, {
---    { name = 'buffer' },
---  })
---})
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
