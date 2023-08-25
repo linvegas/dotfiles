1dc4144b2df5d3eeda801a82f1b45f58e5918dd1
MisterConscio <saulo.costam@gmail.com>
2023-08-24 21:16:28 -0300

    unecessary variable

--- mpv/.config/mpv/input.sh
+++ mpv/.config/mpv/input.sh
@@ -6,11 +6,12 @@ file_name=$(
   echo '{ "command": ["get_property", "path"]}' |
     socat - "/tmp/mpv-socket" | jq -r '.data'
 )
-work_dir=$(
-  echo '{"command": ["get_property", "working-directory"]}' |
-    socat - "/tmp/mpv-socket" | jq -r '.data'
-)
 
-echo "${work_dir}/${file_name}" | xclip -r -sel clip &&
+# work_dir=$(
+#   echo '{"command": ["get_property", "working-directory"]}' |
+#     socat - "/tmp/mpv-socket" | jq -r '.data'
+# )
+
+echo "${file_name}" | xclip -r -sel clip &&
   notify-send "MPV" "File copied"
 
