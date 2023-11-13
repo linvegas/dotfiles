3839469ee9d7e1b26f732b25ca72c15310c50e10
MisterConscio <saulo.costam@gmail.com>
2023-11-13 19:53:28 -0300

    First emacs config :O

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,12 @@
+(setq inhibit-startup-screen t)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(load-theme 'wombat t)
+
+(set-frame-font "DroidSansM Nerd Font Mono 13" nil t)
+
+(setq backup-directory-alist '(("." . "~/.local/share/emacs/backups")))
+(setq eshell-directory-name "~/.local/share/emacs/eshell")
+(setq auto-save-default nil)
