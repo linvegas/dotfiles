fb35de93d1a7aab518913464ff519119f59354a1
MisterConscio <saulo.costam@gmail.com>
2023-11-21 16:40:02 -0300

    indentation space as 4 and setting listchars

--- nvim/.config/nvim/lua/conscio/options.lua
+++ nvim/.config/nvim/lua/conscio/options.lua
@@ -10,11 +10,15 @@ vim.opt.scrolloff = 8
 vim.opt.laststatus = 3
 
 -- Indentation
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+
+-- Special characters display
+vim.opt.listchars = "trail:•,extends:>,leadmultispace:│   "
+vim.opt.list = true
 
 -- Split window
 vim.opt.splitbelow = true
