f799572b5bfaed08a99d65d1e259514df326eb01
linvegas <saulo.costam@gmail.com>
2023-12-23 11:59:07 -0300

    more sane autocomplete setup

--- nvim/.config/nvim/lua/conscio/plugins/cmp.lua
+++ nvim/.config/nvim/lua/conscio/plugins/cmp.lua
@@ -4,9 +4,9 @@ local luasnip = require 'luasnip'
 luasnip.config.setup {}
 
 cmp.setup {
-  -- completion = {
-  --   autocomplete = false,
-  -- },
+  completion = {
+    autocomplete = false,
+  },
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
@@ -17,7 +17,7 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<C-Space>'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
@@ -46,3 +46,4 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
