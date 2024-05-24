c50bb567dbee2a2286e46887fa00f9d4c29d85dc
linvegas <saulo.costam@gmail.com>
2024-05-23 23:38:26 -0300

    New mapping for shell files

--- nvim/.config/nvim/after/ftplugin/sh.lua
+++ nvim/.config/nvim/after/ftplugin/sh.lua
@@ -1,3 +1,4 @@
 vim.keymap.set( "n", "<leader>r", ":split term://sh %<CR>", { remap = true, buffer = true, silent = true })
 vim.keymap.set( "n", "<leader>R", ":split term://sh -x %<CR>", { remap = true, buffer = true, silent = true })
 vim.keymap.set( "n", "<leader>c", ":!shellcheck %<CR>", { remap = true, buffer = true, silent = true })
+vim.keymap.set( "n", "<leader>C", ":!shellcheck -x %<CR>", { remap = true, buffer = true, silent = true })
