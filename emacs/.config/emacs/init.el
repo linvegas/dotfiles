0adf6ba30b3ee51b7a6f61c5fd955672890c3955
linvegas <saulo.costam@gmail.com>
2024-07-05 11:51:54 -0300

    Stupid custom overwriting my config

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -27,3 +27,15 @@
 
 (unless (package-installed-p 'typescript-mode)
   (package-install 'typescript-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(typescript-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
